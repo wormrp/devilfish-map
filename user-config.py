782b4d84dbc9e5a35d82aed268d3ce50a28cf28f
Keira Dueck <sylae@calref.net>
2021-01-03 05:33:38 -0800

    pywikibot

---
+++ user-config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# This is a sample file. You should use generate_user_files.py
+# to create your user-config.py file.
+
+
+family = 'wormrp'
+mylang = 'en'
+
+usernames['wormrp']['en']='Huntress'
+password_file = "user-password.py"
+console_encoding = 'utf-8'
