6fed02b2d0ad11922018d2cf55c25647f1e4a24b
Keira Dueck <sylae@calref.net>
2021-09-24 04:09:42 -0700

    i hate powershell

--- uploadMaps.ps1
+++ uploadMaps.ps1
@@ -5,9 +5,8 @@
 # then create a user-password.py: https://www.mediawiki.org/wiki/Manual:Pywikibot/BotPasswords
 # run (to make sure it works): python-qgis pwb/pwb.py login
 
-
 # this isnt 100% needed but...
-python-qgis pwb/pwb.py login
+python pwb/pwb.py login
 
 # grab our revision tag
 $tag = $(git describe --tags --dirty --always)
@@ -23,5 +22,5 @@ Foreach-Object -Parallel {
 		# 'prt-departments.png' {$dest = 'PRT departments.png'}
 		default {return}
 	}
-	Measure-Command { python-qgis pwb/pwb.py upload -always -abortonwarn -ignorewarn:exists -filename:$dest $_.FullName "CLI upload from sylae/devilfish-map tag $tag" | Out-Default }
+	python pwb/pwb.py upload -always -abortonwarn -ignorewarn:exists -filename:$dest $_.FullName "CLI upload from sylae/devilfish-map tag $tag"
 } -ThrottleLimit 32
