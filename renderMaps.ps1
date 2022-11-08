cd4092e24207c128615590a2a2c57dfa613243a5
Keira Dueck <sylae@calref.net>
2022-11-07 17:32:59 -0800

    new qgis path for my comp
    nobody else uses these scripts so i get to do whatever i want neener neener

--- renderMaps.ps1
+++ renderMaps.ps1
@@ -1,7 +1,7 @@
 # this is designed to run on keira's computer in particular. it shouldn't be *too* difficult to change, however.
 # note that to export... i think all you'll need tbh is uhhh the Roadgeek 2014 fonts, plus probably changing a few env vars somewhere
 
-$python = "C:\Program Files\QGIS 3.20.3\bin\python-qgis"
+$python = "C:\Program Files\QGIS 3.28.0\bin\python-qgis"
 
 Remove-Item "export" -Recurse -ErrorAction Ignore
 mkdir "export" | out-null
