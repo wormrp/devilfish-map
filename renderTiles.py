4868303d63739fc5e8edc37e1f9227cba622f92a
Keira Dueck <sylae@calref.net>
2022-11-07 17:23:18 -0800

    Update renderTiles.py

--- renderTiles.py
+++ renderTiles.py
@@ -18,6 +18,6 @@ Processing.initialize()
 project = QgsProject.instance()
 project.read(sys.argv[1])
 
-processing.run("qgis:tilesxyzdirectory", {'EXTENT':'-91.145624973,-89.211017226,47.356527267,48.282137008 [EPSG:4326]','ZOOM_MIN':10,'ZOOM_MAX':13,'DPI':96,'BACKGROUND_COLOR':QColor(255, 255, 255, 255),'TILE_FORMAT':0,'QUALITY':75,'METATILESIZE':4,'TILE_WIDTH':256,'TILE_HEIGHT':256,'TMS_CONVENTION':False,'OUTPUT_DIRECTORY':'export-tiles','OUTPUT_HTML':'export-tiles/map.tpl.html'})
+processing.run("qgis:tilesxyzdirectory", {'EXTENT':'-91.145624973,-89.211017226,47.356527267,48.282137008 [EPSG:4326]','ZOOM_MIN':10,'ZOOM_MAX':14,'DPI':96,'BACKGROUND_COLOR':QColor(255, 255, 255, 255),'TILE_FORMAT':0,'QUALITY':75,'METATILESIZE':4,'TILE_WIDTH':256,'TILE_HEIGHT':256,'TMS_CONVENTION':False,'OUTPUT_DIRECTORY':'export-tiles','OUTPUT_HTML':'export-tiles/map.tpl.html'})
 
 qgs.exitQgis()
